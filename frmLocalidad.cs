10294ee6f1618c741f4b92982d154616f0202ffc
thiagowpp <thiagowendler53@gmail.com>
2022-09-27 21:49:30 -0300

    Cambios

--- frmLocalidad.cs
+++ frmLocalidad.cs
@@ -21,20 +21,23 @@ namespace pryParedesSP1H1
 
         private void btnCrearLocalidad_Click(object sender, EventArgs e)
         {
+            //Se crea el archivo (./localidad.txt)
             File.Create("./localidad.txt");
 
 
-            
+
         }
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
 
+            //Abro el Agregado de datos, y uso StreamWriter (salida de caracteres en una codificacion determinada)
             TextWriter Escribir = new StreamWriter("./localidad.txt", true);
 
+            //Uso la variable "Escribir" para escribir en el archivo (./localidad.txt)
             Escribir.WriteLine(Convert.ToString(txtIdentificador.Text + " " + txtLocalidad.Text));
             Escribir.Close();
-            
+
 
             MessageBox.Show("La localidad se guardo correctamente", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
@@ -42,11 +45,13 @@ namespace pryParedesSP1H1
             txtLocalidad.Text = "";
             txtLocalidad.Focus();
 
-            
+
+
         }
 
         private void txtLocalidad_TextChanged(object sender, EventArgs e)
         {
+            //Si la textBoxLocalidad es diferente a nada, los botones btnAceptar y btnEliminar se activan
             if (txtLocalidad.Text != "")
             {
                 btnAceptar.Enabled = true;
@@ -54,28 +59,31 @@ namespace pryParedesSP1H1
                 btnEliminar.Enabled = true;
             }
 
-            
+
         }
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
+            //DialogResult, que se basa en el resultado que el usuario de cuando se muestre la MessageBox (Resultados de dialogo: OK - Cancelar)
             DialogResult r = MessageBox.Show("¿Esta seguro que desea eliminar el archivo?" + "\nAtencion: Si elimina el archivo todo el contenido se perdera" + "\n¿Aun desea continuar?", "Atencion", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
 
-            if  (r == DialogResult.OK)
+            if (r == DialogResult.OK)
             {
+                //Elimina el archivo 
                 File.Delete("./localidad.txt");
 
-
+                //MessageBox para avisar que se elimino el archivo
                 MessageBox.Show("Archivo eliminado con exito", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
+            //Si el resultado del dialogo es "Cancel", el archivo no se elimina
             if (r == DialogResult.Cancel)
             {
                 MessageBox.Show("Accion cancelada", "Cancelado", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
 
-            
+
         }
 
         private void frmLocalidad_Load(object sender, EventArgs e)
